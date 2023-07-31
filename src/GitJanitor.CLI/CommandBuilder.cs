6091d3398e7d00269813a96356572554b3d7dab8
Tim Lange <delangetimm@gmail.com>
2023-07-31 20:49:32 +0200

    LibGit2Sharp not needed inside of CommandBuilder

--- src/GitJanitor.CLI/CommandBuilder.cs
+++ src/GitJanitor.CLI/CommandBuilder.cs
@@ -3,7 +3,6 @@ using GitJanitor.Common.Enums;
 using GitJanitor.Common.Models;
 using GitJanitor.Core.Interfaces;
 using GitJanitor.IO.Interfaces;
-using LibGit2Sharp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
